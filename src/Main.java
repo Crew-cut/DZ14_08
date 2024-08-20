d12c7a1a293f6108d135c63328b163fed90f5379
DZ14_08 <Crew-cut93@yandex.by>
2024-08-20 18:25:19 +0300

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        NewArrayList<Integer> integer = new NewArrayList<>();
+        NewLinkedList<Integer> integer = new NewLinkedList<>();
         integer.add(123);
         integer.add(0,2);
         integer.add(5);
-        integer.trimToSize();
+//        integer.trimToSize();
         System.out.println("elem = " + integer);
     }
 }
--- src/NewLinkedList.java
+++ src/NewLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class NewLinkedList <E> {
 
 //    private static final Object [] EMPTY_OBJECT = {};
@@ -8,6 +10,41 @@ public class NewLinkedList <E> {
 
     public NewLinkedList(){
     }
+    public void add (int index, E element){
+        if(index == size){
+            linkLast(element);
+        }else {
+            linkPrev(element, node(index));
+        }
+    }
+
+    public boolean add (E element){
+        linkLast(element);
+        return true;
+    }
+
+    private void linkLast(E element) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, element, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+    void linkPrev(E e, Node<E> succ) {
+
+        final Node<E> prev = succ.prev;
+        final Node<E> newNode = new Node<>(prev, e, succ);
+        succ.prev = newNode;
+        if (prev == null)
+            first = newNode;
+        else
+            prev.next = newNode;
+        size++;
+    }
+
     private static class Node <E>{
         E item;
         Node<E> prev;
@@ -16,6 +53,20 @@ public class NewLinkedList <E> {
             this.item = element;
             this.prev = prev;
             this.next = next;
+        }
+    }
+    Node<E> node(int index) {
+
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
         }
     }
 
