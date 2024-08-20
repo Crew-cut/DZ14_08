c05979737177c6cfdfc2802e214402b8ff4c9ac4
DZ14_08 <crew-cut93@yandex.by>
2024-08-20 11:53:21 +0300

    1.0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NewArrayList<Number> ints = new NewArrayList<>();
+        ints.add(123);
+        ints.add(0,2);
+        ints.add(5);
+        ints.remove(0);
+        System.out.println("elem = "+ints);
+    }
+}
---
+++ src/NewArrayList.java
@@ -0,0 +1,149 @@
+import jdk.internal.util.ArraysSupport;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Arrays;
+
+/**
+ * Класс расширяет возможности стандартного класса Array, и позволяет добавлять объекты в любое место массива,
+ * расширять его размеры и удалять элементы из массива, выделять под массивы.
+ */
+public class NewArrayList <E> {
+    /**
+     * Пустой массив
+     */
+    private static final Object [] EMPTY_OBJECTS = {};
+    /**
+     * Стандартный размер массива
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+    /**
+     * Стандартный пустой массив
+     */
+    private static final Object[] DEFAULT_EMPTY_OBJECTS = {};
+    /**
+     * Инстанс класса хранящий данные массива
+     */
+    private Object [] objects;
+    /**
+     * Размер массива
+     */
+    private int size;
+
+    /**
+     * Конструктор для создания объекта класса заданного размера
+     * @param capacity - заданный размер массива     *
+     * @exception IllegalArgumentException исключение
+     */
+    public  NewArrayList ( int capacity) {
+        if (capacity > 0){
+            this.objects = new Object[capacity];
+        } else if (capacity == 0) {
+            this.objects = EMPTY_OBJECTS;
+        }else {
+            throw new IllegalArgumentException ("Illegal capacity "+ capacity);
+        }
+    }
+
+    /**
+     * Конструктор для создания объекта класса без параметров (DEFAULT)
+     */
+    @Contract(pure = true)
+    public NewArrayList() {
+        this.objects = DEFAULT_EMPTY_OBJECTS;
+    }
+
+    /**
+     *
+     * @return размер заданного массива
+     */
+    public int size (){
+        return size;
+    }
+    public boolean trimToSize (Object [] o){
+
+        return true;
+    }
+
+    /**
+     * Метод выдает элемент массива по его индексу
+     * @param i индекс массива
+     * @return элемент массива
+     */
+    public E get (int i) {
+        return (E) objects[i];
+    }
+
+    /**
+     * Внутренний метод который расширяет массив
+     * @param m минимальный размеер массива
+     * @return расширенный массив
+     */
+    private Object [] grow(int m){
+        int oldCapacity = objects.length;
+        if (oldCapacity >0){
+            int newCapacity = ArraysSupport.newLength(oldCapacity,
+                    m - oldCapacity,
+                    oldCapacity >> 1);
+            return objects = Arrays.copyOf(objects,newCapacity);
+        }else {
+            return objects = new Object[Math.max(DEFAULT_CAPACITY, m)];
+        }
+    }
+
+    /**
+     * Внутренний метод для добавления элемента массива
+     * @param e - элемент массива
+     * @param o - объект массива
+     * @param s - размер массива
+     */
+    private void add (E e, Object [] o, int s){
+        if (s == o.length){
+            o = grow(s+1);
+        }
+        objects[s] = e;
+        size = s + 1;
+    }
+
+    /**
+     * Метод добавляет элемент в конец массива
+     * @param e - элемент массива
+     * @return возвращает true по завершению операции
+     */
+    public boolean add(E e) {
+        add(e,objects,size);
+        return true;
+    }
+
+    /**
+     * Метод добавляет элемент в массив по индексу.
+     * @param i - индекс массива
+     * @param e - элемент массива
+     */
+    public void add (int i, E e){
+        final int s = size;
+        Object [] object = this.objects;
+        if (s == object.length){
+            object = grow(s+1);
+        }
+        System.arraycopy(object, i, object,i + 1,s - i);
+        object[i] = e;
+        size = s + 1;
+    }
+
+    /**
+     * Удаляет элемент массива по индексу
+     * @param i индекс
+     * @throws IndexOutOfBoundsException если индекс вне границ (меньше 0 или меньше размера массива)
+     */
+    public void remove (int i) throws IndexOutOfBoundsException {
+        if(i < 0 || i > size) throw new IndexOutOfBoundsException ();
+        Object [] object = objects;
+        int newSize = size - 1;
+        if (newSize > i){
+            System.arraycopy(object,i+1,object,i,newSize);
+        }
+        object [size=newSize] = null;
+    }
+}
